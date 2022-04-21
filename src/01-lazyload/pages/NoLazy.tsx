564c46768a9b6dcb7813dd14d69916424d4bbc7d
jair <jairdeyner730791@gmail.com>
2022-04-21 14:14:00 -0500

    Rutas anidadas listas

---
+++ src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,8 @@
+const NoLazy = () => {
+  return (
+    <div>
+      <h1>No LazyLoad</h1>
+    </div>
+  );
+};
+export default NoLazy;
--- src/01-lazyload/pages/index.ts
+++ src/01-lazyload/pages/index.ts
@@ -1,7 +1,9 @@
 import LazyPage1 from "./LazyPage1";
 import LazyPage2 from "./LazyPage2";
 import LazyPage3 from "./LazyPage3";
+import NoLazy from "./NoLazy";
 
 export { LazyPage1 };
 export { LazyPage2 };
 export { LazyPage3 };
+export { NoLazy };
